4e556286359a831cd3bc495afdc5ca1693d0056c
xXAreizaXx <jorgeareiza2001@gmail.com>
2022-07-15 21:20:59 -0500

    New Merge

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -3,7 +3,7 @@ import './Cards.css';
 export default function Cards(props) {
     return (
         <div className="Cards" key={props.id}>
-            <h3 className="titleName">{props.name}</h3>
+            <h4 className="titleName">{props.name}</h4>
             <img className="Image" src={props.image} alt={`Imagen de ${props.name}`} />
         </div>
 
