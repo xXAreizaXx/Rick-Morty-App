72084f3b638d127a070a3704e5cab69b4950097f
xXAreizaXx <jorgeareiza2001@gmail.com>
2022-07-15 21:19:22 -0500

    Fix/Line Master

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -3,7 +3,7 @@ import './Cards.css';
 export default function Cards(props) {
     return (
         <div className="Cards" key={props.id}>
-            <h3 className="titleName">{props.name}</h3>
+            <h2 className="titleName">{props.name}</h2>
             <img className="Image" src={props.image} alt={`Imagen de ${props.name}`} />
         </div>
 
